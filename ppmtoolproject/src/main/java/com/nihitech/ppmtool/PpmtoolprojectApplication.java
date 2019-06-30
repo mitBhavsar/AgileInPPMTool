1e09b5c90d15f364281d73889ce6a13b28d16f9c
Mitesh S Bhavsar <mitesh.s.bhavsar@gmail.com>
2019-06-30 06:21:23 +0530

    Add files to git repo

---
+++ ppmtoolproject/src/main/java/com/nihitech/ppmtool/PpmtoolprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nihitech.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolprojectApplication.class, args);
+	}
+
+}
